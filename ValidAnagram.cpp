20887ddb008ecc8a597af5b00c7ed1f9cb2373fc
Keith Bentham <keithbentham92@gmail.com>
2017-09-16 22:07:42 +0100

    no message

---
+++ ValidAnagram.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Keith Bentham
+//
+
+#include <iostream>
+
+class ValidAnagram {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        int n = s.length();
+        int counts[26] = {0};
+        for (int i = 0; i < n; i++) {
+            counts[s[i] - 'a']++;
+            counts[t[i] - 'a']--;
+        }
+        for (int i = 0; i < 26; i++)
+            if (counts[i]) return false;
+        return true;
+    }
+
+    int main(){
+        ValidAnagram va;
+        std::cout << va.isAnagram("rat", "cat");
+    }
+};
